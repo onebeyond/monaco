68b7e225bfd18ea42b6775f8ffcc2e511a0ca635
César Demicheli <cesar.demicheli@dcsl.com>
2022-05-15 18:32:05 +0200

    FIX: Remove cleanup script in favor of Nuget exclusion filter.

--- CleanTemplate.cmd
+++
@@ -1,6 +0,0 @@
-FOR /d /r src\Content %%d IN (bin) DO @IF EXIST %%d rd /s /q %%d
-FOR /d /r src\Content %%d IN (obj) DO @IF EXIST %%d rd /s /q %%d
-FOR /d /r src\Content %%d IN (logs) DO @IF EXIST %%d rd /s /q %%d
-FOR /d /r src\Content %%d IN (.idea) DO @IF EXIST %%d rd /s /q %%d
-FOR /d /r src\Content %%d IN (.vs) DO @IF EXIST %%d rd /s /q %%d
-FOR /r src\Content %%d IN (*.user) DO del %%d
