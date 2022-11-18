df719a34ef349d733d7132f05ca4d3b45da02cc6
Alberto <alberto_tureganocsb@hotmail.com>
2022-11-18 19:47:15 +0100

    Commit staged

--- src/Content/Monaco.Template.Common.Application/DTOs/BaseTypeDto.cs
+++ src/Content/Monaco.Template.Common.Application/DTOs/BaseTypeDto.cs
@@ -2,5 +2,6 @@ namespace Monaco.Template.Common.Application.DTOs;
 
 public class BaseTypeDto
 {
-	
+	public Guid Id { get; set; }
+	public string Name { get; set; }
 }
--- src/Content/Monaco.Template.Common.Application/DTOs/Extensions/BaseTypeExtensions.cs
+++ src/Content/Monaco.Template.Common.Application/DTOs/Extensions/BaseTypeExtensions.cs
@@ -1,6 +1,32 @@
+using Monaco.Template.Common.Domain.Model;
+using Monaco.Template.Common.Domain.Model.Contracts;
+using System.Linq.Expressions;
+
 namespace Monaco.Template.Common.Application.DTOs.Extensions;
 
-public class BaseTypeExtensions
+public static class BaseTypeExtensions
 {
-	
+	public static TResult? Map<TEntity, TResult>(this TEntity? value) 
+		where TEntity : Enumeration, IReferential
+		where TResult : BaseTypeDto, new()
+	{
+		if (value == null)
+			return null;
+
+		return new TResult
+			   {
+				   Id = value.Id,
+				   Name = value.Name
+			   };
+	}
+
+
+	public static Dictionary<string, Expression<Func<TEntity, object>>> GetMappedFields<TEntity>() where TEntity : Enumeration, IReferential
+	{
+		return new()
+			   {
+				   { nameof(Enumeration.Id), x => x.Id },
+				   { nameof(Enumeration.Name), x => x.Name }
+			   };
+	}
 }
