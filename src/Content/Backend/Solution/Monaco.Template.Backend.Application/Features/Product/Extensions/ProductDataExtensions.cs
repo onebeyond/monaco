2a85556421d4ebcc0b0d05d6e3c46e92bebd32e7
CesarD <cesar.demicheli@gmail.com>
2023-12-21 13:09:06 +0100

    CHORE: Optimizing company retrieval query.

--- src/Content/Backend/Solution/Monaco.Template.Backend.Application/Features/Product/Extensions/ProductDataExtensions.cs
+++ src/Content/Backend/Solution/Monaco.Template.Backend.Application/Features/Product/Extensions/ProductDataExtensions.cs
@@ -1,6 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Monaco.Template.Backend.Application.Infrastructure.Context;
-using Monaco.Template.Backend.Common.Infrastructure.Context.Extensions;
 
 namespace Monaco.Template.Backend.Application.Features.Product.Extensions;
 
@@ -11,7 +10,9 @@ public static class ProductDataExtensions
 																					 Guid[] pictures,
 																					 CancellationToken cancellationToken)
 	{
-		var company = await dbContext.GetAsync<Domain.Model.Company>(companyId, cancellationToken);
+		var company = await dbContext.Set<Domain.Model.Company>()
+									 .Include(x => x.Products)
+									 .SingleAsync(x => x.Id == companyId, cancellationToken);
 		var pics = await dbContext.Set<Domain.Model.Image>()
 								  .Include(x => x.Thumbnail)
 								  .Where(x => pictures.Contains(x.Id))
